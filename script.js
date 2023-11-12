94e174f01a2654a42358adc9517ad07d077d7275
Mo Ansari <paytakhtdota@gmail.com>
2023-11-11 18:12:03 -0800

    random freelancers added

--- script.js
+++ script.js
@@ -5,6 +5,24 @@ const freelancers = [
     { name: "Ashli", occupation: "Designer", startingPrice: 60 }
   ];
   
+  function generateFreelancer() {
+    const poolNames = ["John", "Michael", "David", "James", "William", "Robert", "Richard", "Charles", "Joseph", "Mary", "Elizabeth", "Jennifer", "Linda", "Susan", "Margaret", "Barbara", "Dorothy", "Sarah"];
+    const poolOccupations = ["Writer", "Teacher", "Programmer", "Designer", "Doctor", "Lawyer", "Accountant", "Engineer", "Salesperson", "Manager", "Customer Service Representative", "Web Developer", "Graphic Designer", "Photographer", "Videographer", "Animator", "Musician"];
+    const name = poolNames[Math.floor(Math.random() * poolNames.length)];
+    const occupation = poolOccupations[Math.floor(Math.random() * poolOccupations.length)];
+    const startingPrice = Math.floor(Math.random() * 126) + 25;
+  
+    return {
+      name,
+      occupation,
+      startingPrice,
+    };
+  }
+ function addFreelancer(){
+    freelancers.push(generateFreelancer());}
+ setInterval(addFreelancer,3000);
+ 
+  
   const body = document.body;
   body.style = "display:flex; justify-content:center;";
   
@@ -14,11 +32,11 @@ const freelancers = [
   mainDiv.style = "border : solid black 2px;width : 500px; height: 250;margin-top:10%";
   
   // Add the grid style
-  mainDiv.style.display = "grid";
-  mainDiv.style.gridTemplateColumns = "repeat(1, 1fr)";
-  mainDiv.style.gridTemplateRows = "repeat(3, 45px)";
+  mainDiv.style.display = "flex";
+  mainDiv.style.flexDirection = "column";
   mainDiv.style.alignItems = "center";
   mainDiv.style.justifyItems = "center";
+  mainDiv.style.gridGap = "10px";
   
   console.log(mainDiv);
   body.appendChild(mainDiv);
@@ -26,30 +44,35 @@ const freelancers = [
   mainDiv.appendChild(document.createElement("h1")).innerHTML = "Freelancer Forum";
   mainDiv.appendChild(document.createElement("h3")).innerHTML = "The average starting price is";
   mainDiv.appendChild(document.createElement("h2")).innerHTML = "Available Freelancers";
-
+  
   const table = document.createElement("table");
+  table.borderCollapse = "collapse";
   const headerRow = document.createElement("tr");
-  headerRow.innerHTML ="<th>Name</th><th>Occupation</th><th>Starting Price</th>";
+  headerRow.innerHTML = `<th>Name&nbsp&nbsp</th><th>Occupation&nbsp&nbsp</th><th> Starting Price</th>`;
   table.appendChild(headerRow);
-
+  
   // Add the table rows
-for (const freelancer of freelancers) {
-    const row = document.createElement("tr");
+  for (let i = 0; i < freelancers.length; i++) {
+    // Add a delay of 2 seconds
+    setTimeout(() => {
+      const row = document.createElement("tr");
   
-    const nameCell = document.createElement("td");
-    nameCell.textContent = freelancer.name;
-    row.appendChild(nameCell);
+      const nameCell = document.createElement("td");
+      nameCell.textContent = freelancers[i].name;
+      row.appendChild(nameCell);
   
-    const occupationCell = document.createElement("td");
-    occupationCell.textContent = freelancer.occupation;
-    row.appendChild(occupationCell);
+      const occupationCell = document.createElement("td");
+      occupationCell.textContent = freelancers[i].occupation;
+      row.appendChild(occupationCell);
   
-    const priceCell = document.createElement("td");
-    priceCell.textContent = freelancer.startingPrice;
-    row.appendChild(priceCell);
+      const priceCell = document.createElement("td");
+      priceCell.textContent = ("$"+freelancers[i].startingPrice);
+      row.appendChild(priceCell);
   
-    table.appendChild(row);
+      table.appendChild(row);
+    }, 2000);
   }
+  
   mainDiv.appendChild(table);
-
-
+  function log(){console.log(freelancers)};
+  setInterval(log,3001);
